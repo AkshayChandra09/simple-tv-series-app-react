327de8829fae72e08c72a10ab1d0e7c65c79f65f
AkshayChandra09 <akshay.chandrachood@gmail.com>
2019-03-03 17:17:06 -0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+
+// const greeting = React.createElement('h1', {}, 'Hello World!');
+
+// const getCurrentDate = function() {
+// 	const date = new Date();
+// 	return date.toDateString();
+// }
+
+// const greeting = <h1>Hello React! Current Date: {getCurrentDate()}</h1>;
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+/// If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
